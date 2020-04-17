ff28367690d375306d86294624844da0e8f0d282
Mathan <capturesmk@gmail.com>
2020-04-17 11:37:14 +0530

    Designs Emoji Cards :lipstick:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+      <>
+
+  <h1 className="has-text-centered" id="headertext">Readmoji</h1>
+  <div className="has-text-centered" id="subtext"><span class="tag is-link" id="emojitext">Emoji Guide for README's</span></div>
+
+  <div class="columns is-multiline is-centered">
+    <div class="column is-narrow has-text-centered">
+      <div class="box">
+        <figure class="image container is-64x64 ">
+          <img class="is-rounded" src="https://github.githubassets.com/images/icons/emoji/bowtie.png?v8" alt="emoji"/>
+        </figure>
+        <span class="tag is-success" id="emojitext">:bowtie:</span>
+      </div>
     </div>
-  );
+  </div>
+
+  <footer className="footer">
+    <div className="content has-text-centered">
+        <p id="footertext">
+            &lt;/&gt; with ❤️ by <a href="https://github.com/capturemathan">Mathan</a>
+        </p>
+    </div>
+  </footer>
+</>
+    )
+  }
 }
 
-export default App;
+export default App
