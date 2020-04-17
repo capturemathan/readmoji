73382831eb2b0807a2d1e7b61290187e64b3e1a8
capturemathan <capturesmk@gmail.com>
2020-04-17 15:14:05 +0530

    Updates

---
+++ precache-manifest.6ced13cad897e0d4c8bfe55ef4c156d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566bb76cd6fe0bbf5f358941c385f5a7",
+    "url": "/readmoji/index.html"
+  },
+  {
+    "revision": "91b587ce2bbbf7892ee3",
+    "url": "/readmoji/static/css/main.5b14ee1e.chunk.css"
+  },
+  {
+    "revision": "4346b4c3e6302444a4eb",
+    "url": "/readmoji/static/js/2.73c05a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/readmoji/static/js/2.73c05a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b587ce2bbbf7892ee3",
+    "url": "/readmoji/static/js/main.51664cc8.chunk.js"
+  },
+  {
+    "revision": "764e275afb3c53a94e4d",
+    "url": "/readmoji/static/js/runtime-main.d85dab41.js"
+  },
+  {
+    "revision": "8ad80587ca1ee2052ce7f25119dbba74",
+    "url": "/readmoji/static/media/loadimg.8ad80587.png"
+  }
+]);
